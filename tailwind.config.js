2835a8cbe49f905647693bcd491507e818981323
Diego ROJAS <rojasdiegopro@gmail.com>
2022-10-25 19:10:59 +0800

    feat: base styles for headers and spacing
    Signed-off-by: Diego ROJAS <rojasdiegopro@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        '2xl': '8rem',
+        'xl': '6rem',
+        'lg': '4rem',
+        'md': '3rem',
+        'sm': '2rem',
+        'xs': '1rem',
+        'xxs': '0.5rem',
+      }
+    }
   },
   plugins: [],
 }
