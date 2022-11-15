c5b35ad015bebdbf38d5e8eb4e183c8c873df50b
kfleury <killian.fleury@epitech.eu>
2022-11-15 16:06:55 +0900

    feat: notes pages with columns and preview of the notes

--- src/views/notes/NotesView.tsx
+++ src/views/notes/NotesView.tsx
@@ -1,23 +1,53 @@
 import React from 'react'
+import {fakeGroup} from './fakeNotesData'
 import {fakeNotes} from './fakeNotesData'
-import groupLogo from './multiple-users-silhouette.png'
+import groupLogo from '../../styles/icon/multiple-users-silhouette.png'
+import NoteCard from '../../components/card/NoteCard'
 
 const NotesView: React.FC = () => {
-  const notes = fakeNotes
+  const notes = fakeGroup
 
-  return (<div className='flex bg-gray-800 text-center text-white'>
+  const noteList = fakeNotes
+
+  return (<div><div className='flex border border-rounded rounded-full bg-gray-800 justify-center text-center text-white'>
     <div
-      className="group relative w-full justify-center py-2 px-4 text-xl font-medium"
+      className="flex-auto group relative w-40 justify-center py-2 px-4 text-xl font-medium"
     >
       <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-        <img className='flex-auto' src={groupLogo} alt='group icon'
-          height='20px' width='20px'></img>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+          className="w-6 h-6">
+          <path strokeLinecap="round" strokeLinejoin="round"
+            d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"/>
+        </svg>
+
       </span>
       {notes.groupName}
     </div>
-    <div className='flex-auto w-20'>{notes.notesNbr} notes</div>
-    <div className='flex-auto w-20'>{notes.creationDate}</div>
-    <div className='flex-auto w-40'>{notes.modifiedDate}</div>
+    <div className='flex-auto justify-center py-2 w-20'><span className="flex item-center"><svg
+      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+      className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round"
+        d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"/>
+    </svg>
+    {notes.notesNbr} notes</span></div>
+    <div className='flex-auto justify-center py-2 w-40'><span className="flex item-center"><svg
+      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+      className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+    </svg>
+    {notes.creationDate}</span></div>
+    <div className='flex-auto justify-center py-2 w-40'><span className="flex item-center"><svg
+      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+      className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+    </svg>
+    {notes.modifiedDate}</span></div>
+  </div>
+  <div className="grid grid-cols-1 gap-1 md:grid-cols-2 md:gap-2 lg:grid-cols-4 lg:gap-4">
+    { noteList.map((e) => {
+      return <NoteCard key={e.name} name={e.name} firstChars={e.firstChars}/>
+    })}
+  </div>
   </div>)
 }
 
--- src/views/notes/fakeNotesData.ts
+++ src/views/notes/fakeNotesData.ts
@@ -5,9 +5,24 @@ export interface INotes {
   modifiedDate: string;
 }
 
-export const fakeNotes: INotes = {
+export interface INote {
+  name: string;
+  firstChars: string;
+}
+
+export const fakeGroup: INotes = {
   groupName: 'Maths',
   notesNbr: 15,
   creationDate: '15/10/2022',
   modifiedDate: '15/10/2022 : 13h55'
 }
+
+export const fakeNotes: INote[] = [
+  {name: 'Arithmétique 1', firstChars: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum in metus id porttitor. Phasellus.'},
+  {name: 'Arithmétique 2', firstChars: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum in metus id porttitor. Phasellus.'},
+  {name: 'Blabla 1', firstChars: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum in metus id porttitor. Phasellus.'},
+  {name: 'Blibli 1', firstChars: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum in metus id porttitor. Phasellus.'},
+  {name: 'Kakao 1', firstChars: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum in metus id porttitor. Phasellus.'},
+  {name: 'Musique 1', firstChars: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum in metus id porttitor. Phasellus.'},
+  {name: 'Physique 1', firstChars: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum in metus id porttitor. Phasellus.'}
+]
