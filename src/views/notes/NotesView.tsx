84b5533684236377c7da969a08399ab9c1c45ee9
kfleury <killian.fleury@epitech.eu>
2022-11-15 19:51:59 +0900

    feat: tests on the second topbar

--- src/views/notes/NotesView.tsx
+++ src/views/notes/NotesView.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {fakeGroup} from './fakeNotesData'
 import {fakeNotes} from './fakeNotesData'
-import groupLogo from '../../styles/icon/multiple-users-silhouette.png'
 import NoteCard from '../../components/card/NoteCard'
 import NoteRec from '../../components/card/NoteRec'
 
@@ -10,7 +9,7 @@ const NotesView: React.FC = () => {
 
   const noteList = fakeNotes
 
-  const notesHeader = (
+  const notesHeaderUp = (
     <div className='flex border border-rounded rounded-full bg-gray-800 justify-center text-center text-white'>
       <div
         className="flex-auto group relative w-40 justify-center py-2 px-4 text-xl font-medium"
@@ -47,8 +46,45 @@ const NotesView: React.FC = () => {
       {notes.modifiedDate}</span></div>
     </div>)
 
+  const notesHeaderDown = (
+    <div className='flex border h-30 border-rounded rounded-full bg-gray-800 justify-center text-center text-white'>
+      <div
+        className="flex-auto group relative w-40 justify-center py-2 px-4 text-xl font-medium"
+      >
+        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round"
+              d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"/>
+          </svg>
+
+        </span>
+        {notes.groupName}
+      </div>
+      <div className='flex-auto justify-center py-2 w-20'><span className="flex item-center"><svg
+        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+        className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round"
+          d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125"/>
+      </svg>
+      {notes.notesNbr} notes</span></div>
+      <div className='flex-auto justify-center py-2 w-40'><span className="flex item-center"><svg
+        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+        className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+      </svg>
+      {notes.creationDate}</span></div>
+      <div className='flex-auto justify-center py-2 w-40'><span className="flex item-center"><svg
+        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
+        className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"/>
+      </svg>
+      {notes.modifiedDate}</span></div>
+    </div>)
+
   const colNotesView = (<div>
-    {notesHeader}
+    {notesHeaderUp}
     <div className="grid grid-cols-1 gap-1 md:grid-cols-2 md:gap-2 lg:grid-cols-4 lg:gap-4">
       {noteList.map((e) => {
         return <NoteCard key={e.name} name={e.name} firstChars={e.firstChars}/>
@@ -57,7 +93,7 @@ const NotesView: React.FC = () => {
   </div>)
 
   const rowNotesView = (<div>
-    {notesHeader}
+    {notesHeaderDown}
     <div className="grid grid-rows-1 gap-1">
       {noteList.map((e) => {
         return <NoteRec key={e.name} name={e.name} authorName={e.authorName}/>
