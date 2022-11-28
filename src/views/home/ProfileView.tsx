fda8a2ad662ddca7a22f5d8482a71f546d4ce5b5
Diego ROJAS <rojasdiegopro@gmail.com>
2022-11-28 19:57:01 +0800

    feat: panel view anti affinity
    Signed-off-by: Diego ROJAS <rojasdiegopro@gmail.com>

--- src/views/home/ProfileView.tsx
+++ src/views/home/ProfileView.tsx
@@ -1,7 +1,20 @@
 import React from 'react'
 import DashboardHeader from '../../components/view/DashboardHeader'
+import { TPanelKey, usePanelContext } from '../../contexts/panel'
 
 const ProfileView: React.FC = () => {
+  const profileViewPanels: TPanelKey[] = ['group-overview', 'group-chat','group-settings']
+  const { activePanel, setActivePanel, panels, setPanels } = usePanelContext()
+
+  React.useEffect(() => {
+    if (profileViewPanels !== panels) {
+      setPanels(profileViewPanels)
+    }
+    if (!profileViewPanels.includes(activePanel)) {
+      setActivePanel(profileViewPanels[0])
+    }
+  }, [])
+
   return <React.Fragment>
     <DashboardHeader>
       <h1>Profile</h1>
