53f4d5ef1fdf195c48ed6e14ef470f9236748454
Diego ROJAS <rojasdiegopro@gmail.com>
2022-11-28 21:46:28 +0800

    feat: dashboard header
    Signed-off-by: Diego ROJAS <rojasdiegopro@gmail.com>

--- src/components/view/DashboardHeader.tsx
+++ src/components/view/DashboardHeader.tsx
@@ -1,14 +1,24 @@
+import { ChevronDownIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 
 const DashboardHeader: React.FC<React.PropsWithChildren> = (props) => {
   return <div className='h-[36px] mt-xl mx-lg xl:mx-xl flex items-center justify-between'>
     {props.children}
     <div className='flex items-center'>
+      <div className='rounded border border-gray-300 text-sm flex cursor-pointer'>
+        <div className='font-medium text-gray-800 flex items-center'>
+          <img className='h-8 w-8 p-[1px] mr-[3px]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJjxQSevxZSdV9xXy7LWCvgmJTAKaeaF3Kj0GWU_FrypKqrlN3xZOD9r125HbaO9SauK0&usqp=CAU' />
+          Noted 2022
+        </div>
+        <div className='border-l border-gray-300 flex items-center justify-center px-1 ml-2'>
+          <ChevronDownIcon className='h-4 w-4 stroke-[10px] text-gray-500' />
+        </div>
+      </div>
       {/* <div className='bg-blue-200 h-6 px-2 rounded-full text-blue-600 flex items-center'>
         <span className='text-xs'>Synced</span>
         <CheckIcon className='ml-xxs h-3 w-3 text-blue-600 stroke-[2.5px]' />
       </div> */}
-      <img src='https://avatars.githubusercontent.com/u/63368264?v=4' className='ml-md h-[40px] w-[40px] xl:h-[48px] xl:w-[48px] bg-gray-100 rounded-full shadow border border-white'></img>
+      {/* <img src='https://avatars.githubusercontent.com/u/63368264?v=4' className='ml-md h-[40px] w-[40px] xl:h-[48px] xl:w-[48px] bg-gray-100 rounded-full shadow border border-white'></img> */}
     </div>
   </div>
 }
--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -18,11 +18,12 @@ const HomeView: React.FC = () => {
 
   return <div>
     <DashboardHeader>
-      <div className='cursor-pointer flex items-center rounded-full hover:bg-gray-100'>
+      {/* <div className='cursor-pointer flex items-center rounded-full hover:bg-gray-100'>
         <img className='h-[36px] w-[36px] bg-gray-100 rounded-full border border-gray-300' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJjxQSevxZSdV9xXy7LWCvgmJTAKaeaF3Kj0GWU_FrypKqrlN3xZOD9r125HbaO9SauK0&usqp=CAU' />
         <h1 className='ml-md'>Noted 2022</h1>
         <ChevronDownIcon className='ml-md h-4 w-4 stroke-[3px] text-gray-500 mr-md' />
-      </div>
+      </div> */}
+      <h2>Home</h2>
     </DashboardHeader>
     <div className='flex'>
       <div className='border-2 border-dashed border-gray-300 h-48 m-lg xl:m-xl p-xl w-full flex items-center justify-center text-gray-400'>View Body</div>
