4682af9296cc121c30535fb8262b1a0ed29c4704
Diego ROJAS <rojasdiegopro@gmail.com>
2022-12-26 23:00:56 +0800

    feat: create group empty state
    Signed-off-by: Diego ROJAS <rojasdiegopro@gmail.com>

--- src/components/view/DashboardHeader.tsx
+++ src/components/view/DashboardHeader.tsx
@@ -1,12 +1,25 @@
 import { ChevronDownIcon } from '@heroicons/react/24/solid'
 import React from 'react'
+import { useGroupContext } from '../../contexts/group'
 
 const GroupSelectInput: React.FC = () => {
+  const groupContext = useGroupContext()
+
   return <div className='flex items-center'>
     <div className='rounded border border-gray-300 text-sm flex cursor-pointer'>
-      <div className='font-medium text-gray-800 flex items-center'>
-        <img className='h-8 w-8 p-[1px] mr-[3px]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJjxQSevxZSdV9xXy7LWCvgmJTAKaeaF3Kj0GWU_FrypKqrlN3xZOD9r125HbaO9SauK0&usqp=CAU' />
-    Noted 2022
+      <div className='font-medium text-gray-800 flex items-center h-[34px]'>
+        {
+          groupContext.groupID === null ? 
+            <React.Fragment>
+              <div className='h-7 w-7 ml-1 bg-gray-200 rounded' />
+              <p className='text-gray-700 text-sm font-normal pl-1'>Select a group</p>
+            </React.Fragment>
+            :
+            <React.Fragment>
+              <img className='h-8 w-8 p-[1px] mr-[3px]' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJjxQSevxZSdV9xXy7LWCvgmJTAKaeaF3Kj0GWU_FrypKqrlN3xZOD9r125HbaO9SauK0&usqp=CAU' />
+              Noted 2022
+            </React.Fragment>
+        }
       </div>
       <div className='border-l border-gray-300 flex items-center justify-center px-1 ml-2'>
         <ChevronDownIcon className='h-4 w-4 stroke-[10px] text-gray-500' />
--- src/views/home/HomeView.tsx
+++ src/views/home/HomeView.tsx
@@ -1,9 +1,31 @@
+import { PlusIcon, UserPlusIcon } from '@heroicons/react/24/outline'
 import React from 'react'
 import ViewSkeleton from '../../components/view/ViewSkeleton'
+import { useGroupContext } from '../../contexts/group'
+
+const NoGroupEmptyState: React.FC = () => {
+  return <div className='flex w-full h-full flex-col items-center pt-12'>
+    <UserPlusIcon className='h-12 w-12 text-gray-400 stroke-1' />
+    <p className='font-medium text-gray-700 mt-4'>No Group</p>
+    <p className='text-sm text-gray-500'>Create or join a group to start writing!</p>
+    <button className='bg-blue-600 text-white px-4 p-2 rounded-full text-sm flex items-center mt-4'>
+      <PlusIcon className='mr-1 h-4 w-4 stroke-2' />
+      Create group
+    </button>
+    <p className='font-medium text-gray-700 mt-6'>Invites</p>
+    <div className='text-gray-500 text-sm mt-2'>
+      Invites to groups will show up here.
+    </div>
+  </div>
+}
 
 const HomeView: React.FC = () => {
+  const groupContext = useGroupContext()
+
   return <ViewSkeleton title='Home' panels={['group-overview', 'group-chat','group-settings']}>
-    <div className='border-2 border-dashed border-gray-300 mb-lg xl:mb-xl h-[1024px] mx-lg xl:mx-xl w-full flex items-center justify-center text-gray-400'>View Body</div>
+    <div className='mb-lg xl:mb-xl mx-lg xl:mx-xl w-full flex items-center justify-center'>
+      {groupContext.groupID === null ? <NoGroupEmptyState /> : <div>Full State</div>}
+    </div>
   </ViewSkeleton>
 }
 
