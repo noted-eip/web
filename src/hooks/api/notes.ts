b9116b2f635949da34b04718d41116f1943475d9
Diego ROJAS <rojasdiegopro@gmail.com>
2023-02-21 16:56:48 +0800

    feat: delete note context menu

--- src/hooks/api/notes.ts
+++ src/hooks/api/notes.ts
@@ -19,6 +19,8 @@ export const useCreateNoteInCurrentGroup = (options?: MutationHookOptions<Create
     ...options,
     onSuccess: async (data, variables, context) => {
       apiQueryClient.setQueryData(newNoteCacheKey(groupContext.groupId as string, data.note.id), data)
+      apiQueryClient.invalidateQueries(newNotesCacheKey())
+
       if (options?.onSuccess) options.onSuccess(data, variables, context)
     }
   })
@@ -53,5 +55,46 @@ export const useListNotes = (req: ListNotesRequest, options?: QueryHookOptions<L
       return (await openapiClient.notesAPIListNotes(req.groupId, req.authorAccountId, req.limit, req.offset, await axiosRequestOptionsWithAuthorization(authContext))).data
     },
     ...options,
+  })
+}
+
+export type DeleteNoteRequestInCurrentGroup = { noteId: string };
+export const useDeleteNoteInCurrentGroup = (options?: MutationHookOptions<DeleteNoteRequestInCurrentGroup, object>) => {
+  const authContext = useAuthContext()
+  const groupContext = useGroupContext() 
+  const currentGroupId = groupContext.groupId as string
+
+  return useMutation(async (req: DeleteNoteRequestInCurrentGroup) => {
+    return (await openapiClient.notesAPIDeleteNote(currentGroupId, req.noteId, await axiosRequestOptionsWithAuthorization(authContext))).data
+  },
+  {
+    ...options,
+    // Optimistically remove the note from each notes array.
+    onMutate: async (variables) => {
+      await apiQueryClient.cancelQueries(newNotesCacheKey())
+
+      const previousNotes = apiQueryClient.getQueryData(newNotesCacheKey({ groupId: currentGroupId }))
+      // @ts-expect-error previous check.
+      apiQueryClient.setQueriesData(newNotesCacheKey(), (old: V1ListNotesResponse) => {
+        if (!old) return old
+        return {notes: old.notes?.filter((note) => note.id !== variables.noteId)}
+      })
+
+      if (options?.onMutate) options.onMutate(variables)
+      return { previousNotes }
+    },
+    onError: (error, variables, context) => {
+      if (context?.previousNotes) {
+        apiQueryClient.setQueryData(newNotesCacheKey(), context.previousNotes)
+      }
+
+      if (options?.onError) options.onError(error, variables, context)
+    },
+    onSettled: async (data, error, variables, context) => {
+      // Always refetch.
+      apiQueryClient.invalidateQueries(newNotesCacheKey({ groupId: currentGroupId }))
+  
+      if (options?.onSettled) options.onSettled(data, error, variables, context)
+    }
   })
 }
--- src/views/group/GroupViewNotesTab.tsx
+++ src/views/group/GroupViewNotesTab.tsx
@@ -1,26 +1,99 @@
+import { LinkIcon as LinkIconOutline, PencilIcon as PencilIconOutline, TrashIcon as TrashIconOutline } from '@heroicons/react/24/outline'
 import { FolderIcon, PencilIcon } from '@heroicons/react/24/solid'
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useGetAccount } from '../../hooks/api/accounts'
 import { useGetCurrentGroup } from '../../hooks/api/groups'
-import { useCreateNoteInCurrentGroup, useListNotesInCurrentGroup } from '../../hooks/api/notes'
+import { useCreateNoteInCurrentGroup, useDeleteNoteInCurrentGroup, useListNotesInCurrentGroup } from '../../hooks/api/notes'
 import { V1Note } from '../../protorepo/openapi/typescript-axios'
 import GroupViewMenu from './GroupViewMenu'
 
+type NotesLiNotesListGridItemContextMenuProps = {
+  note: V1Note,
+  targetId: string,
+  options: {
+    name: string,
+    icon: (props: React.SVGProps<SVGSVGElement> & { title?: string | undefined; titleId?: string | undefined; }) => JSX.Element,
+    onClick: () => void,
+  }[],
+}
+
+// TODO: Extract to re-usable context menu component.
+const NotesListGridItemContextMenu: React.FC<NotesLiNotesListGridItemContextMenuProps> = (props) => {
+  const [contextData, setContextData] = React.useState({ visible:false, posX: 0, posY: 0})
+  const contextRef = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    const contextMenuEventHandler= (e) => {
+      const targetElement = document.getElementById(props.targetId)
+
+      if (targetElement && targetElement.contains(e.target)) {
+        e.preventDefault()
+        setContextData({ visible: true, posX: e.clientX, posY: e.clientY })
+      } else if(contextRef.current && !contextRef.current.contains(e.target)){
+        setContextData({ ...contextData, visible: false })
+      }
+    }
+
+    const offClickHandler= () => {
+      if(contextRef.current){
+        setContextData({ ...contextData, visible: false })
+      }
+    }
+
+    document.addEventListener('contextmenu', contextMenuEventHandler)
+    document.addEventListener('click', offClickHandler)
+    return () => {
+      document.removeEventListener('contextmenu', contextMenuEventHandler)
+      document.removeEventListener('click', offClickHandler)
+    }
+  }, [contextData, props.targetId])
+
+  return <div ref={contextRef} className='absolute w-48 list-none rounded-md border border-gray-200 bg-white shadow-lg' style={{ display:`${contextData.visible ? 'block' : 'none'}`, left: contextData.posX, top: contextData.posY }}>
+    <div className=''>
+      {props.options.map((option) => (
+        <li
+          key={`notes-list-grid-item-context-menu-${props.note.id}-${option.name}`}
+          className='group m-1 flex cursor-pointer items-center rounded-md px-3 py-2 text-sm hover:bg-gray-50'
+          onClick={() => {option.onClick()}}>
+          <div className='mr-2'>
+            {<option.icon className='h-5 w-5 stroke-2 text-gray-400 group-hover:text-gray-500' />}
+          </div>
+          <p className='text-gray-600 group-hover:text-black'>
+            {option.name}
+          </p>
+        </li>
+      ))}
+    </div>
+  </div>
+}
+
 const NotesListGridItem: React.FC<{ note: V1Note }> = (props) => {
   const authorQ = useGetAccount({accountId: props.note.authorAccountId})
+  const deleteNoteQ = useDeleteNoteInCurrentGroup()
   const navigate = useNavigate()
 
   return (
-    <div
-      className='flex h-48 w-full cursor-pointer flex-col items-center justify-center rounded-md border border-gray-100 bg-gray-50 p-2 transition-all hover:bg-gray-100 hover:shadow-inner'
-      onClick={() => navigate(`./note/${props.note.id}`)}
-    >
-      <div className='mb-2 h-2/3 w-1/2 rounded-md bg-white shadow-md' />
-      <div className='w-full text-center'>
-        <div className='text-xs font-medium text-gray-800'>{props.note.title}</div>
-        <p className='text-xxs text-gray-500'>{authorQ.data?.account.name}</p>
+    <div>
+      <div
+        className='flex h-48 w-full cursor-pointer flex-col items-center justify-center rounded-md border border-gray-100 bg-gray-50 p-2 transition-all hover:bg-gray-100 hover:shadow-inner'
+        onClick={() => navigate(`./note/${props.note.id}`)}
+        id={`group-view-notes-tab-grid-${props.note.id}`}
+      >
+        <div className='mb-2 h-2/3 w-1/2 rounded-md bg-white shadow-md' />
+        <div className='w-full text-center'>
+          <div className='text-xs font-medium text-gray-800'>{props.note.title}</div>
+          <p className='text-xxs text-gray-500'>{authorQ.data?.account.name}</p>
+        </div>
       </div>
+      <NotesListGridItemContextMenu
+        targetId={`group-view-notes-tab-grid-${props.note.id}`}
+        options={[
+          {icon: PencilIconOutline, name: 'Rename', onClick: () => { /* TODO */ }},
+          {icon: LinkIconOutline, name: 'Copy Link', onClick: () => { /* TODO */ }},
+          {icon: TrashIconOutline, name: 'Delete', onClick: () => { deleteNoteQ.mutate({ noteId: props.note.id }) }},
+        ]}
+        note={props.note} />
     </div>
   )
 }
