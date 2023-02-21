adfa72c3b754e3791b6c4c37f6d2f51bf8daad7b
Diego ROJAS <rojasdiegopro@gmail.com>
2023-02-21 22:28:37 +0800

    feat: update note title

--- src/hooks/api/notes.ts
+++ src/hooks/api/notes.ts
@@ -2,7 +2,7 @@ import { useMutation, useQuery } from 'react-query'
 import { useAuthContext } from '../../contexts/auth'
 import { useGroupContext } from '../../contexts/group'
 import { apiQueryClient, openapiClient } from '../../lib/api'
-import { NotesAPICreateNoteRequest, V1CreateNoteResponse, V1ListNotesResponse } from '../../protorepo/openapi/typescript-axios'
+import { NotesAPICreateNoteRequest, V1CreateNoteResponse, V1GetNoteResponse, V1ListNotesResponse, V1Note, V1UpdateNoteResponse } from '../../protorepo/openapi/typescript-axios'
 import { newNoteCacheKey, newNotesCacheKey } from './cache'
 import { axiosRequestOptionsWithAuthorization, MutationHookOptions, QueryHookOptions } from './helpers'
 
@@ -38,6 +38,22 @@ export const useListNotesInCurrentGroup = (req: ListNotesInCurrentGroupRequest, 
     queryKey: queryKey,
     queryFn: async () => {
       return (await openapiClient.notesAPIListNotes2(currentGroupId, req.authorAccountId, req.limit, req.offset, await axiosRequestOptionsWithAuthorization(authContext))).data
+    },
+    ...options,
+  })
+}
+
+export type GetNoteInCurrentGroupRequest = { noteId: string }
+export const useGetNoteInCurrentGroup = (req: GetNoteInCurrentGroupRequest, options?: QueryHookOptions<GetNoteInCurrentGroupRequest, V1GetNoteResponse>) => {
+  const authContext = useAuthContext()
+  const groupContext = useGroupContext()
+  const currentGroupId = groupContext.groupId as string
+  const queryKey = newNoteCacheKey(currentGroupId, req.noteId)
+
+  return useQuery({
+    queryKey: queryKey,
+    queryFn: async () => {
+      return (await openapiClient.notesAPIGetNote(currentGroupId, req.noteId, await axiosRequestOptionsWithAuthorization(authContext))).data
     },
     ...options,
   })
@@ -93,6 +109,54 @@ export const useDeleteNoteInCurrentGroup = (options?: MutationHookOptions<Delete
     onSettled: async (data, error, variables, context) => {
       // Always refetch.
       apiQueryClient.invalidateQueries(newNotesCacheKey({ groupId: currentGroupId }))
+  
+      if (options?.onSettled) options.onSettled(data, error, variables, context)
+    }
+  })
+}
+
+export type UpdateNoteInCurrentGroup = { noteId: string, body: V1Note };
+export const useUpdateNoteInCurrentGroup = (options?: MutationHookOptions<UpdateNoteInCurrentGroup, V1UpdateNoteResponse>) => {
+  const authContext = useAuthContext()
+  const groupContext = useGroupContext() 
+  const currentGroupId = groupContext.groupId as string
+
+  return useMutation(async (req: UpdateNoteInCurrentGroup) => {
+    return (await openapiClient.notesAPIUpdateNote(currentGroupId, req.noteId, req.body, undefined, await axiosRequestOptionsWithAuthorization(authContext))).data
+  },
+  {
+    ...options,
+    // Optimistically update the note everywhere.
+    onMutate: async (variables) => {
+      await apiQueryClient.cancelQueries(newNotesCacheKey())
+      await apiQueryClient.cancelQueries(newNoteCacheKey(currentGroupId, variables.noteId))
+
+      const previousNote = apiQueryClient.getQueryData(newNoteCacheKey(currentGroupId, variables.noteId))
+      // @ts-expect-error previous check.
+      apiQueryClient.setQueriesData(newNoteCacheKey(currentGroupId, variables.noteId), (old: V1GetNoteResponse) => {
+        if (!old) return old
+        return {note: {...old.note, ...variables.body}}
+      })
+      // @ts-expect-error previous check.
+      apiQueryClient.setQueriesData(newNotesCacheKey(), (old: V1ListNotesResponse) => {
+        if (!old) return old
+        return {notes: old.notes?.map((note) => note.id === variables.noteId ? {...note, ...variables.body} : note)}
+      })
+
+      if (options?.onMutate) options.onMutate(variables)
+      return { previousNote }
+    },
+    onError: (error, variables, context) => {
+      if (context?.previousNote) {
+        apiQueryClient.setQueryData(newNoteCacheKey(currentGroupId, variables.noteId), context.previousNote)
+      }
+
+      if (options?.onError) options.onError(error, variables, context)
+    },
+    onSettled: async (data, error, variables, context) => {
+      // Always refetch.
+      apiQueryClient.invalidateQueries(newNotesCacheKey())
+      apiQueryClient.invalidateQueries(newNoteCacheKey(currentGroupId, variables.noteId))
   
       if (options?.onSettled) options.onSettled(data, error, variables, context)
     }
--- src/views/note/NotesView.tsx
+++ src/views/note/NotesView.tsx
@@ -1,34 +1,78 @@
 import React from 'react'
+import ContentEditable from 'react-contenteditable'
 import ViewSkeleton from '../../components/view/ViewSkeleton'
+import { useGetNoteInCurrentGroup, useUpdateNoteInCurrentGroup } from '../../hooks/api/notes'
+import { V1Note } from '../../protorepo/openapi/typescript-axios'
 
 const NoteViewHeader: React.FC = () => {
+  const [latestTitle, setLatestTitle] = React.useState<string>()
+  const noteId = location.pathname.split('/')[4]
+  const getNoteQ = useGetNoteInCurrentGroup({ noteId })
+  const updateNoteQ = useUpdateNoteInCurrentGroup()
+  const text = React.useRef<string | undefined>()
+
+  React.useEffect(() => {
+    if (getNoteQ.isSuccess && !text.current) {
+      text.current = getNoteQ.data.note.title
+      setLatestTitle(getNoteQ.data.note.title)
+    }
+  }, [getNoteQ])
+
+  const handleChange = evt => {
+    text.current = evt.target.value
+  }
+
+  const handleBlur = () => {
+    updateNote()
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault()
+      updateNote()
+    }
+  }
+
+  const updateNote = () => {
+    if (text.current == latestTitle) {
+      return
+    }
+    setLatestTitle(text.current)
+    updateNoteQ.mutate({ noteId, body: { title: text.current || '' } as V1Note})
+  }
+
   return <div className='flex items-center justify-center'>
     <div className='flex h-8 w-8 items-center justify-center rounded-md bg-gray-100'>📝</div>
-    <div className='ml-2 w-96 rounded-md px-1 text-lg font-medium text-gray-600 ring-blue-200 focus:outline-none focus:ring-2' contentEditable>World War II</div>
+    {
+      text.current ?
+        <ContentEditable
+          className='ml-2 w-96 rounded-md px-1 text-lg font-medium text-gray-600 ring-blue-200 focus:outline-none focus:ring-2'
+          html={text.current}
+          onBlur={handleBlur}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          contentEditable />
+        :
+        <div className='skeleton ml-2 h-4 w-32' />
+    }
   </div>
 }
 
 const NoteView: React.FC = () => {
   return <ViewSkeleton titleElement={<NoteViewHeader />} panels={['group-chat', 'group-activity', 'note-recommendations']}>
-    <div className='px-lg pb-lg focus:outline-none xl:px-xl xl:pb-2xl' >
-      {/* Heading 1 */}
+    {/* <div className='px-lg pb-lg focus:outline-none xl:px-xl xl:pb-2xl' >
       <h1 className='pt-8 text-3xl font-medium text-gray-800 first:pt-0'>The Pre-Industrial Era</h1>
-      {/* Paragraph */}
       <div className='pt-2 text-justify'>The Industrial Revolution was a major turning point in human history, marked by a profound shift from manual labor to machine-based manufacturing. It began in the mid-18th century in Britain and quickly spread throughout Europe and North America, fundamentally transforming the way goods were produced, distributed, and consumed.</div>
       <div className='pt-2 text-justify'> The Industrial Revolution brought about numerous technological innovations, such as the steam engine and mechanized textile production, which dramatically increased the efficiency and productivity of factories.</div>
-      {/* Heading 2 */}
       <h2 className='pt-6 text-2xl font-medium text-gray-800'>Cottage industries and handicraft production</h2>
       <div className='pt-2 text-justify'>The Industrial Revolution was a major turning point in human history, marked by a profound shift from manual labor to machine-based manufacturing. It began in the mid-18th century in Britain and quickly spread throughout Europe and North America, fundamentally transforming the way goods were produced, distributed, and consumed.</div>
-      {/* Heading 3 */}
       <h2 className='pt-5 text-xl font-medium text-gray-800'>Limitations of pre-industrial production</h2>
       <div className='pt-2 text-justify'>The Industrial Revolution was a major turning point in human history, marked by a profound shift from manual labor to machine-based manufacturing. It began in the mid-18th century in Britain and quickly spread throughout Europe and North America, fundamentally transforming the way goods were produced, distributed, and consumed.</div>
-      {/* Heading 3 */}
       <h2 className='pt-5 text-xl font-medium text-gray-800'>Some other headline thing</h2>
       <div className='pt-2 text-justify'>The Industrial Revolution was a major turning point in human history, marked by a profound shift from manual labor to machine-based manufacturing. It began in the mid-18th century in Britain and quickly spread throughout Europe and North America, fundamentally transforming the way goods were produced, distributed, and consumed.</div>
-      {/* Heading 3 */}
       <h2 className='pt-5 text-xl font-medium text-gray-800'>Say sike right now</h2>
       <div className='pt-2 text-justify'>The Industrial Revolution was a major turning point in human history, marked by a profound shift from manual labor to machine-based manufacturing. It began in the mid-18th century in Britain and quickly spread throughout Europe and North America, fundamentally transforming the way goods were produced, distributed, and consumed.</div>
-    </div>
+    </div> */}
   </ViewSkeleton>
 }
 
