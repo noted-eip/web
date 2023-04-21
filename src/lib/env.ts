d5ef59411b1b5ea2b70f45bcdc0ee442091dfbd1
kfleury <killian.fleury@epitech.eu>
2023-04-21 22:00:45 +0900

    fix: env.ts put again the url of the website

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,4 +1,4 @@
 export const API_BASE: string =
-  process.env.REACT_APP_API_BASE || 'https://web-desktop-git-queries-caching-and-more-noted-eip.vercel.app'
+  process.env.REACT_APP_API_BASE || 'https://noted-rojasdiego.koyeb.app'
 
 export const TOGGLE_DEV_FEATURES = !!process.env.REACT_APP_TOGGLE_DEV_FEATURES || false
