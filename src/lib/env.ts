de3b45710a1af7581945a5c793b739243d89a932
kfleury <killian.fleury@epitech.eu>
2023-05-15 19:20:49 +0900

    feat: put the google client id

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,7 @@
 export const API_BASE: string =
   process.env.REACT_APP_API_BASE || 'https://noted-rojasdiego.koyeb.app'
 
-export const TOGGLE_DEV_FEATURES = !!process.env.REACT_APP_TOGGLE_DEV_FEATURES || false
-
 export const GOOGLE_CLIENT_ID: string =
-  process.env.REACT_GOOGLE_CLIENT_ID || ''
+  process.env.REACT_GOOGLE_CLIENT_ID || '871625340195-kf7c2u88u9aivgdru776a36hgel0kjja.apps.googleusercontent.com'
+  
+export const TOGGLE_DEV_FEATURES = !!process.env.REACT_APP_TOGGLE_DEV_FEATURES || false
