a2e41a2201d5c58dafe54c777b319c601fd81498
kfleury <killian.fleury@epitech.eu>
2023-08-16 18:01:55 +0200

    feat: technical documentation

--- src/views/welcome/WelcomeView.tsx
+++ src/views/welcome/WelcomeView.tsx
@@ -10,7 +10,7 @@ import TimelineLanding from '../../components/view/landing/TimelineLanding'
 const WelcomeView: React.FC = () => {
   const analytics = getAnalytics()
   
-  logEvent(analytics, 'bite')
+  logEvent(analytics, 'testWelcomeView')
   return (
     <div className='bg-white dark:bg-gray-900'>
       <HeaderLanding />
