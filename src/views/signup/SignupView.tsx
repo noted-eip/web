66af29ec27ae0a5ea6186f45d3d7807162573ceb
Killian Fleury <killian.fleury@epitech.eu>
2023-11-15 15:20:48 +0100

    feat: signup page with mui

--- src/views/signup/SignupView.tsx
+++ src/views/signup/SignupView.tsx
@@ -1,3 +1,5 @@
+import Button from '@mui/material/Button'
+import Stack from '@mui/material/Stack'
 import { useGoogleLogin } from '@react-oauth/google'
 import { getAnalytics, logEvent } from 'firebase/analytics'
 import React from 'react'
@@ -6,6 +8,7 @@ import { useNavigate } from 'react-router-dom'
 
 import ContainerMd from '../../components/container/ContainerMd'
 import OldInput from '../../components/form/OldInput'
+import GoogleIcon from '../../components/icons/GoogleIcon'
 import Notification from '../../components/notification/Notification'
 import { addAccountToDevelopmentContext, useDevelopmentContext } from '../../contexts/dev'
 import { useNoAuthContext } from '../../contexts/noauth'
@@ -87,7 +90,6 @@ const SignupView: React.FC = () => {
   return (
     <div className='flex h-screen w-screen items-center justify-center'>
       <form
-        className='grid basis-1/2 grid-cols-1 gap-2'
         onSubmit={(e) => {
           e.preventDefault()
           createAccountMutation.mutate({body: {name, email, password}}, )
@@ -97,94 +99,73 @@ const SignupView: React.FC = () => {
           <h2 className='mb-4 text-xl font-bold leading-tight tracking-tight text-gray-900 dark:text-white md:text-2xl'>
             <FormatMessage id='SIGNUP.title' />
           </h2>
-          <OldInput
-            label={formatMessage({ id: 'GENERIC.name' })}
-            value={name}
-            onChange={(e) => {
-              const val = e.target.value as string
-              setName(val)
-              setNameValid(validateName(val) === undefined)
-            }}
-            isInvalidBlur={!nameValid}
-            errorMessage='Invalid name'
-          />
-          <OldInput
-            label={formatMessage({ id: 'AUTH.email' })}
-            value={email}
-            onChange={(e) => {
-              const val = e.target.value as string
-              setEmail(val)
-              setEmailValid(validateEmail(val) === undefined)
-            }}
-            isInvalidBlur={!emailValid}
-            errorMessage='Invalid email address'
-          />
-          {/** //TODO: remove the type=passwprd of the input **/}
-          <OldInput
-            label={formatMessage({ id: 'AUTH.pwd' })}
-            tooltip='6 characters, letters numbers and symbols'
-            value={password}
-            onChange={(e) => {
-              const val = e.target.value as string
-              setPassword(val)
-              setPasswordValid(validatePassword(val) === undefined)
-            }}
-            isInvalidBlur={!passwordValid}
-          />
-          <div className='relative my-5 mx-10'>
-            <p className='text-sm italic text-gray-500'>
+          <Stack direction='column' spacing={2}>
+            <OldInput
+              label={formatMessage({ id: 'GENERIC.name' })}
+              value={name}
+              onChange={(e) => {
+                const val = e.target.value as string
+                setName(val)
+                setNameValid(validateName(val) === undefined)
+              }}
+              isInvalidBlur={!nameValid}
+              errorMessage='Invalid name'
+            />
+            <OldInput
+              label={formatMessage({ id: 'AUTH.email' })}
+              value={email}
+              onChange={(e) => {
+                const val = e.target.value as string
+                setEmail(val)
+                setEmailValid(validateEmail(val) === undefined)
+              }}
+              isInvalidBlur={!emailValid}
+              errorMessage='Invalid email address'
+            />
+            <OldInput
+              label={formatMessage({ id: 'AUTH.pwd' })}
+              tooltip='6 characters, letters numbers and symbols'
+              value={password}
+              onChange={(e) => {
+                const val = e.target.value as string
+                setPassword(val)
+                setPasswordValid(validatePassword(val) === undefined)
+              }}
+              isInvalidBlur={!passwordValid}
+            />
+            
+            <div className='relative my-5 mx-10'>
+              <p className='text-sm italic text-gray-500'>
               Once registered, you can ask an early access to our mobile&apos;s
               app through your account&apos;s settings !
-            </p>
-            <p className='text-xs italic text-gray-400'>
+              </p>
+              <p className='text-xs italic text-gray-400'>
               Only works for emails linked to a Google account.
-            </p>
-            <span className='absolute right-auto top-0 -left-2 -translate-y-1/2 -translate-x-1/2 -rotate-12 rounded-full bg-red-400 p-0.5 px-2 text-center text-xs font-medium leading-none text-white outline outline-red-100 dark:bg-blue-900 dark:text-blue-200'>
+              </p>
+              <span className='absolute right-auto top-0 -left-2 -translate-y-1/2 -translate-x-1/2 -rotate-12 rounded-full bg-red-400 p-0.5 px-2 text-center text-xs font-medium leading-none text-white outline outline-red-100 dark:bg-blue-900 dark:text-blue-200'>
               BETA
-            </span>
-          </div>
-          <button
-            className='my-2 w-full rounded-lg bg-blue-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-blue-700 focus:outline-none focus:ring-4 focus:ring-blue-300 disabled:bg-gray-600 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-            disabled={
-              !formIsValid() ||
-            authenticateMutation.isLoading ||
-            createAccountMutation.isLoading
-            }
-          >
-            <FormatMessage id='AUTH.register' />
-          </button>
-          <div style={{cursor: 'pointer'}} className='flex items-center justify-center rounded border border-gray-500 bg-white px-3 py-2 text-sm font-medium text-gray-800 dark:border-gray-500 dark:bg-gray-400'
-            onClick={() => googleLogin()}>
-            <p className='mr-2 dark:text-gray-400'>
-              <FormatMessage id='SIGNUP.signupGoogle' />
-            </p>
-            <svg
-              className='h-5 w-5'
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              width='24'
-              height='24'
+              </span>
+            </div>
+            <Button
+              variant='contained'
+              className='w-full'
+              disabled={
+                !formIsValid() ||
+                authenticateMutation.isLoading ||
+                createAccountMutation.isLoading    
+              }
             >
-              <g transform='matrix(1, 0, 0, 1, 27.009001, -39.238998)'>
-                <path
-                  fill='#4285F4'
-                  d='M -3.264 51.509 C -3.264 50.719 -3.334 49.969 -3.454 49.239 L -14.754 49.239 L -14.754 53.749 L -8.284 53.749 C -8.574 55.229 -9.424 56.479 -10.684 57.329 L -10.684 60.329 L -6.824 60.329 C -4.564 58.239 -3.264 55.159 -3.264 51.509 Z'
-                />
-                <path
-                  fill='#34A853'
-                  d='M -14.754 63.239 C -11.514 63.239 -8.804 62.159 -6.824 60.329 L -10.684 57.329 C -11.764 58.049 -13.134 58.489 -14.754 58.489 C -17.884 58.489 -20.534 56.379 -21.484 53.529 L -25.464 53.529 L -25.464 56.619 C -23.494 60.539 -19.444 63.239 -14.754 63.239 Z'
-                />
-                <path
-                  fill='#FBBC05'
-                  d='M -21.484 53.529 C -21.734 52.809 -21.864 52.039 -21.864 51.239 C -21.864 50.439 -21.724 49.669 -21.484 48.949 L -21.484 45.859 L -25.464 45.859 C -26.284 47.479 -26.754 49.299 -26.754 51.239 C -26.754 53.179 -26.284 54.999 -25.464 56.619 L -21.484 53.529 Z'
-                />
-                <path
-                  fill='#EA4335'
-                  d='M -14.754 43.989 C -12.984 43.989 -11.404 44.599 -10.154 45.789 L -6.734 42.369 C -8.804 40.429 -11.514 39.239 -14.754 39.239 C -19.444 39.239 -23.494 41.939 -25.464 45.859 L -21.484 48.949 C -20.534 46.099 -17.884 43.989 -14.754 43.989 Z'
-                />
-              </g>
-            </svg>
-          </div>
+              <FormatMessage id='AUTH.register' />
+            </Button>
+            <Button 
+              variant='outlined'
+              className='w-full'
+              onClick={() => googleLogin()}
+              endIcon={<GoogleIcon />}
+            >
+              <FormatMessage id='SIGNUP.signupGoogle' />
+            </Button>
+          </Stack>
         </ContainerMd>
       </form>
       <Notification />
