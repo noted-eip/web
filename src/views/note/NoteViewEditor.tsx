cadd06825569c91b166b5d11de32e17e878677d6
Gabriel <gabriel.medoukali@epitech.eu>
2023-12-07 14:08:34 +0100

    feat: not working editor

--- src/views/note/NoteViewEditor.tsx
+++ src/views/note/NoteViewEditor.tsx
@@ -4,9 +4,9 @@ import { withHistory } from 'slate-history'
 import { Editable, RenderElementProps, Slate,withReact } from 'slate-react'
 
 import { useAuthContext } from '../../contexts/auth'
-import { useInsertBlockInCurrentGroup,useUpdateBlockInCurrentGroup } from '../../hooks/api/notes'
+import {useInsertBlockInCurrentGroup,useUpdateBlockInCurrentGroup } from '../../hooks/api/notes'
 import {noteBlocksToSlateElements,stringToNoteBlock,withShortcuts } from '../../lib/editor'
-import { NotesAPIInsertBlockRequest,V1Block, V1Note } from '../../protorepo/openapi/typescript-axios'
+import {NotesAPIInsertBlockRequest,V1Block, V1Note } from '../../protorepo/openapi/typescript-axios'
 
 
 const EditorElement: React.FC<RenderElementProps> = props => {
@@ -30,8 +30,7 @@ const EditorElement: React.FC<RenderElementProps> = props => {
   }
 }
 
-
-const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlocks: React.Dispatch<React.SetStateAction<V1Block[]>> }> = props => {
+const NoteViewEditor: React.FC<{ note: V1Note }> = props => {
   const authContext = useAuthContext()
 
   const initialEditorState = noteBlocksToSlateElements(props.note?.blocks ?? [] as V1Block[])
@@ -43,8 +42,10 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
   const updateBlockMutation = useUpdateBlockInCurrentGroup()
   //const deleteBlockMutation = useDeleteBlockInCurrentGroup()
 
+  //console.log('OUT FUNCTION LOCALBLOCKS = ');console.log(localBlocks)
+
   const insertBlock = async (notedId: string, index: number | undefined, block: V1Block) => {
-    console.log('----INSERT')
+    //console.log('----INSERT')
     const res = await insertBlockMutation.mutateAsync({
       noteId: notedId,
       body: { 
@@ -56,7 +57,7 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
   }
 
   const updateBlock = (notedId: string, blockId: string | undefined, block: V1Block) => {
-    console.log('----UPDATE')
+    //console.log('----UPDATE')
     updateBlockMutation.mutate({
       noteId:  notedId,
       blockId: blockId == undefined ? '' : blockId,
@@ -84,26 +85,86 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
     return null
   }
 
+  const [localBlocks, setLocalBlocks] = React.useState<V1Block[]>( props.note?.blocks ?? [])
+  React.useEffect(() => { setLocalBlocks((prevLocalBlocks) => prevLocalBlocks ?? [])}, [localBlocks])
+
   const updateNoteBlocksFromSlateValue = useCallback(
     async (value: Descendant[]) => {
       const lines = value as any
-      const currentBlockIndex = getLineFromCursorPath(getCurrentCursorPath(editor))
+      const cursorLine = getLineFromCursorPath(getCurrentCursorPath(editor))
 
-      const updateBlockContent = lines[currentBlockIndex].children[0].text
-      
-      props.setLocalBlocks((prevLocalBlocks) => {
-        const newBlocks = [...prevLocalBlocks]
-        if (newBlocks[currentBlockIndex] != undefined) {
-          console.log('(1)=====LINES = '); console.log(lines)
-          console.log('=> Update block idx: ' + currentBlockIndex + '  with : ', updateBlockContent)
-          newBlocks[currentBlockIndex].paragraph = updateBlockContent
-          updateBlock(props.note.id, newBlocks[currentBlockIndex].id, stringToNoteBlock(updateBlockContent))
+      //const insertedBlockId = await insertBlock(props.note.id, cursorLine, stringToNoteBlock(contentInsertBlock))
+      setLocalBlocks((prevLocalBlocks) => {
+        if (prevLocalBlocks.length < lines.length) {
+          console.log('--->Insert New Line & Update Block')
+  
+          const contentUpdateBlock = lines[cursorLine - 1].children[0].text
+          const contentInsertBlock = lines[cursorLine].children[0].text
+          const insertedBlockId = await insertBlock(props.note.id, cursorLine, stringToNoteBlock(contentInsertBlock))
+          const insertedLocalBlock = { id: insertedBlockId, type: 'TYPE_PARAGRAPH', paragraph: contentInsertBlock } as V1Block
+          
+          // LOCALBLOCKS doesn't upatde ...
+          //setLocalBlocks((prevLocalBlocks) => {
+          console.log('LocalBlocks in SetLocalBlocks 1 = '); console.log(prevLocalBlocks)
+          const newBlocks = [...prevLocalBlocks]
+          newBlocks[cursorLine - 1].paragraph = contentUpdateBlock
+          newBlocks.splice(cursorLine, 0, insertedLocalBlock)
+          return newBlocks
+          //})
+  
+        } else {
+          console.log('--->Update Block')
+        
+          const updateBlockContent = lines[cursorLine].children[0].text
+          //setLocalBlocks((prevLocalBlocks) => {
+          console.log('LocalBlocks in SetLocalBlocks 2 = '); console.log(prevLocalBlocks)
+          const newBlocks = [...prevLocalBlocks]
+          if (newBlocks[cursorLine] != undefined) {
+            newBlocks[cursorLine].paragraph = updateBlockContent
+            updateBlock(props.note.id, newBlocks[cursorLine].id, stringToNoteBlock(updateBlockContent))
+          }
+          return newBlocks
+          //})
+  
         }
-        return newBlocks
       })
 
+      /*if (localBlocks.length < lines.length) {
+        console.log('--->Insert New Line & Update Block')
+
+        const contentUpdateBlock = lines[cursorLine - 1].children[0].text
+        const contentInsertBlock = lines[cursorLine].children[0].text
+        const insertedBlockId = await insertBlock(props.note.id, cursorLine, stringToNoteBlock(contentInsertBlock))
+        const insertedLocalBlock = { id: insertedBlockId, type: 'TYPE_PARAGRAPH', paragraph: contentInsertBlock } as V1Block
+        
+        // LOCALBLOCKS doesn't upatde ...
+        setLocalBlocks((prevLocalBlocks) => {
+          console.log('LocalBlocks in SetLocalBlocks 1 = '); console.log(prevLocalBlocks)
+          const newBlocks = [...prevLocalBlocks]
+          newBlocks[cursorLine - 1].paragraph = contentUpdateBlock
+          newBlocks.splice(cursorLine, 0, insertedLocalBlock)
+          return newBlocks
+        })
+
+      } else {
+        console.log('--->Update Block')
+      
+        const updateBlockContent = lines[cursorLine].children[0].text
+        setLocalBlocks((prevLocalBlocks) => {
+          console.log('LocalBlocks in SetLocalBlocks 2 = '); console.log(prevLocalBlocks)
+          const newBlocks = [...prevLocalBlocks]
+          if (newBlocks[cursorLine] != undefined) {
+            newBlocks[cursorLine].paragraph = updateBlockContent
+            updateBlock(props.note.id, newBlocks[cursorLine].id, stringToNoteBlock(updateBlockContent))
+          }
+          return newBlocks
+        })
+
+      }
+      */
+
     },
-    [props]
+    [localBlocks, setLocalBlocks]
   )
 
   const handleEditorChange = (value: Descendant[]) => {
@@ -113,7 +174,7 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
     }
   }
 
-  const handleEnter = async () => {
+  /*const handleEnter = async () => {
     const { selection } = editor
     if (selection == null)
       return
@@ -127,10 +188,10 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
     console.log('(2)========LINES = '); console.log(lines)
     console.log('contentBeforeEnter : ', contentBeforeEnter)
     console.log('contentAfterEnter : ', contentAfterEnter)
-    console.log('BEFORE LocalBlocks'); console.log(props.localBlocks)
+    console.log('BEFORE LocalBlocks'); console.log(localBlocks)
     
     // @note: BACK - UPDATE BLOCK
-    const updatedBlockId = props.localBlocks[cursorLine].id
+    const updatedBlockId = localBlocks[cursorLine].id
     const updatedLocalBlock = { id: updatedBlockId, type: 'TYPE_PARAGRAPH', paragraph: contentBeforeEnter } as V1Block
     updateBlock(props.note.id, updatedBlockId, updatedLocalBlock)
     // @note: BACK - INSERT NEW BLOCK
@@ -140,7 +201,7 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
     
     //fill localBlocks content with lines
 
-    props.setLocalBlocks((prevLocalBlocks) => {
+    setLocalBlocks((prevLocalBlocks) => {
       const newBlocks = [...prevLocalBlocks]
       console.log('AFTER LocalBlocks'); console.log(newBlocks)
       newBlocks[cursorLine].paragraph = contentBeforeEnter
@@ -149,8 +210,7 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
       
       return newBlocks
     })
-
-  }
+  }*/
 
   return (
     <div className='rounded-md bg-transparent bg-gradient-to-br p-4'>
@@ -159,192 +219,14 @@ const NoteEditor: React.FC<{ note: V1Note, localBlocks: V1Block[], setLocalBlock
         editor={editor}
         value={initialEditorState}>
         <Editable
-          onKeyDown= { event => { 
+          /*onKeyDown= { event => { 
             if (event.key == 'Enter') {
               handleEnter()
             }
-          }}
+          }}*/
           readOnly={authContext.accountId !== props.note.authorAccountId}
           renderElement={renderElement} />
       </Slate>
-    </div>
-  )
-}
-
-/*
-const BlockEditorItem: React.FC<{ note: V1Note, block?: V1Block, blockIndex: number, localBlocks: V1Block[], setLocalBlocks: React.Dispatch<React.SetStateAction<V1Block[]>> }> = props => {
-  const authContext = useAuthContext()
-  const blockContext = useBlockContext()
-
-  const initialEditorState = [{ type: 'TYPE_PARAGRAPH', children: [{ text: props.localBlocks[props.blockIndex].paragraph ?? '' }] } as Descendant]
-  const editorState = React.useRef<Descendant[]>(initialEditorState)
-  const renderElement = React.useCallback(props => <EditorElement {...props} />, [])
-  const editor = React.useMemo(() => withShortcuts(withReact(withHistory(createEditor()))), [])
-  
-  const insertBlockMutation = useInsertBlockInCurrentGroup()
-  const updateBlockMutation = useUpdateBlockInCurrentGroup()
-  const deleteBlockMutation = useDeleteBlockInCurrentGroup()
-  
-  editor.children = [{ type: 'TYPE_PARAGRAPH', children: [{ text: props.localBlocks[props.blockIndex]?.paragraph ?? '' }] }]
-
-  
-  const updateBlockFromSlateValue = useCallback(
-    async (value: Descendant[]) => {
-      const lines = value as any
-
-      const newLocalBlocks = [...props.localBlocks]
-
-      newLocalBlocks[props.blockIndex].paragraph = lines[0]?.children[0]?.text ?? ''
-      updateBlock(props.note.id, props.block?.id, stringToNoteBlock(lines[0].children[0].text))
-
-      if (lines[1] != null || lines[1] != undefined) {
-        const newBlockContent = lines[1]?.children[0]?.text ?? ''
-        const newBlockId = await insertBlock(props.note.id, props.blockIndex == undefined ? 1000 : props.blockIndex + 1, stringToNoteBlock(newBlockContent))
-        const newLocalBlock = { id: await newBlockId, type: 'TYPE_PARAGRAPH', paragraph: newBlockContent } as V1Block
-
-        props.setLocalBlocks((prevLocalBlocks) => {
-          const newBlocks = [...prevLocalBlocks]
-          newBlocks.splice(props.blockIndex + 1, 0, newLocalBlock)
-          return newBlocks
-        })
-        
-        editor.children = [{ type: 'TYPE_PARAGRAPH', children: [{ text: lines[0]?.children[0]?.text ?? '' }] }]
-      } else {
-        props.setLocalBlocks((prevLocalBlocks) => [...prevLocalBlocks])
-        editor.children = [{ type: 'TYPE_PARAGRAPH', children: [{ text: lines[0]?.children[0]?.text ?? '' }] }]
-      }
-    },
-    [props]
-  )
-
-  const insertBlock = async (notedId: string, index: number | undefined, block: V1Block) => {
-    console.log('----INSERT')
-    const res = await insertBlockMutation.mutateAsync({
-      noteId: notedId,
-      body: { 
-        index: index, 
-        block: block 
-      } as NotesAPIInsertBlockRequest
-    })
-    return res.block.id
-  }
-
-  const updateBlock = (notedId: string, blockId: string | undefined, block: V1Block) => {
-    console.log('----UPDATE')
-    updateBlockMutation.mutate({
-      noteId:  notedId,
-      blockId: blockId == undefined ? '' : blockId,
-      body: block
-    })
-  }
-
-  const deleteBlock = () => {
-    console.log('----DELETE')
-    if (props.block == undefined)
-      return
-    deleteBlockMutation.mutate({
-      noteId: props.note.id,
-      blockId: props.block.id,
-    })
-  }
-
-  //const handleEnter = () => {
-  //  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  //  const lines = editorState.current as any
-  //  const { selection } = editor
-  //  if (selection == null)
-  //    return
-  //  const cursorRowPosition = selection.focus.path[0]
-  //  const cursorColumnPosition = selection.focus.offset.toString()
-  //  let contentBeforeEnter = ''
-  //  let contentAfterEnter = ''
-  //  for (let i = 0; i < lines.length;++i) {
-  //    for (let j = 0; j < lines[i].children[0].text.length; ++j) {
-  //      if (cursorRowPosition <= i && cursorColumnPosition <= j)
-  //        contentAfterEnter += lines[i].children[0].text[j]
-  //      else
-  //        contentBeforeEnter += lines[i].children[0].text[j]
-  //    }
-  //  }
-  //  updateBlock(props.note.id, props.block == undefined ? '' : props.block.id,  stringToNoteBlock(contentBeforeEnter))
-  //  insertBlock(props.note.id, props.blockIndex == undefined ? 1000 : props.blockIndex + 1,  stringToNoteBlock(contentAfterEnter))
-  //}
-
-  const handleEditorChange = (value: Descendant[]) => {
-    //Gerer plus tard
-    // si Shift & Enter sont pressé, editor.current = "1234\n5678"
-
-    if (editor.operations.some(op => 'set_selection' !== op.type)) {
-      updateBlockFromSlateValue(value)
-    }
-  }
-
-  const handleBackspace = () => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const element = editorState.current[0] as any
-
-    if (element.children[0].text.length < 1) {
-      deleteBlock()
-      //return (<div>No block here<div/>)
-    }
-  }
-
-  const handleHover = () => {
-    if (props.block != undefined) {
-      blockContext.changeBlock(props.block.id)
-    }
-  }
-
-  return (
-    <div 
-      //className='rounded-md bg-transparent bg-gradient-to-br p-4' // none hover
-      //className='rounded-md bg-transparent bg-gradient-to-br p-4 hover:border-gray-100 hover:bg-gray-100 hover:shadow-inner' // block on hover
-      className='rounded-md border-gray-100 bg-gray-100 bg-gradient-to-br p-4 shadow-inner' // just blocks
-      onMouseEnter={handleHover}>
-      <Slate
-        onChange={handleEditorChange}
-        editor={editor}
-        value={initialEditorState}>
-        <Editable
-          onKeyDown= { event => { 
-            if (event.key == 'Backspace') handleBackspace()
-          }}
-          readOnly={authContext.accountId !== props.note.authorAccountId}
-          renderElement={renderElement} />
-      </Slate>
-    </div>
-  )
-}
-*/
-
-//faire que un seul composant pas de parent et fils
-const NoteViewEditor: React.FC<{ note: V1Note }> = props => {
-  const [localBlocks, setLocalBlocks] = React.useState<V1Block[]>( props.note?.blocks ?? [])
-
-  React.useEffect(() => {
-    setLocalBlocks((prevLocalBlocks) => prevLocalBlocks ?? [])
-  }, [localBlocks])
-
-  //console.log('PARENT localBlocks == '); console.log(localBlocks)
-
-  return (
-    <div>
-      {
-        /*localBlocks?.map((block, index) => (
-          <BlockEditorItem key={`block-item-${index}`}
-            note={props.note}
-            block={block}
-            blockIndex={index}
-            localBlocks={localBlocks}
-            setLocalBlocks={setLocalBlocks}
-          />
-        ))*/
-        <NoteEditor
-          note={props.note}
-          localBlocks={localBlocks}
-          setLocalBlocks={setLocalBlocks}
-        />
-      }
     </div>
   )
 }
